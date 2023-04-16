de4f4d8d67ca44685b1da6841a38ac2cd6ee1444
dpatterson8583 <dpatterson8583@gmail.com>
2023-04-15 20:29:52 -0500

    Initial Commit for Methods Exercises

---
+++ MethodsExercise-2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+
+namespace MethodsExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var name = Console.ReadLine();
+
+            var result = Add(10, 20, 5);
+            Console.WriteLine(result);
+
+            var res2 = Subtract(20, 5, 5);
+            Console.WriteLine(res2);
+
+            var res3 = Multiply(1, 2, 3, 4);
+            Console.WriteLine(res3);
+
+            var res4 = Divide(12, 5);
+            Console.WriteLine(res4);
+
+            var res5 = Modulus(23, 5);
+            Console.WriteLine(res5);
+
+            Console.ReadLine();
+        }
+
+        public static int Add(params int[] list)
+        {
+            int res=0;
+
+            for(int i=0; i<list.Length; i++)
+            {
+                res = res + list[i];
+            }
+            return res;
+        }
+
+        public static int Subtract(params int[] list)
+        {
+            //First number is the total to be subtracted from initially.
+            int res = list[0];
+
+            for (int i = 1; i < list.Length; i++)
+            {
+                res = res - list[i];
+            }
+            return res;
+        }
+
+        public static int Multiply(params int[] list)
+        {
+            int res = 1;
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                res = res * list[i];
+            }
+            return res;
+        }
+
+        public static double Divide(params int[] list)
+        {
+            int res = list[0];
+
+            for (int i = 1  ; i < list.Length; i++)
+            {
+                res = res / list[i];
+            }
+            return res;
+        }
+
+        public static int Modulus(params int[] list)
+        {
+            int res = list[0];
+
+            for (int i = 1; i < list.Length; i++)
+            {
+                res = res % list[i];
+            }
+            return res;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string? yourName = "";
+            string? yourAnimal = "";
+            string? yourColor = "";
+            string? yourBand = "";
+
+            yourName = DataEntry("What is your name? > ");
+            yourColor = DataEntry("What is your favorite color? > ");
+            yourAnimal = DataEntry("What is your favorite animal? > ");
+            yourBand = DataEntry("What is your favorite band? >");
+
+            WriteStory(yourName, yourColor, yourAnimal, yourBand);
+        }
+
+        public static void WriteStory(string ynam, string ycol, string yan, string yban)
+        {
+            Console.WriteLine($"My Short Story");
+            Console.WriteLine($"Hi!  My name is {ynam}.");
+            Console.WriteLine($"I grew up collecting {yan}.");
+            Console.WriteLine($"I found that while a {yan} may be {ycol}, not all of them are.");
+            Console.WriteLine($"I played music for the {yan} from {yban} but I don't think they cared.");
+        }
+
+        public static string DataEntry(string msg)
+        {
+            Console.Write(msg);
+            string? field1 = Console.ReadLine();
+            if (field1 == "" || field1 is null )
+            {
+                field1 = "NO VALUE";
+            }
+            return field1;
         }
     }
 }
